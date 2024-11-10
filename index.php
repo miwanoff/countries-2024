4234ae5387559f6e58213fc4179ccdc38029d069
miwanoff <marina.iva.noffa@gmail.com>
2024-11-10 12:41:42 +0200

    Add echo login  password

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once "action.php";
+
+$autorized = false;
+if (isset($_POST["go"])){
+    $login = $_POST["login"];    
+    $password = $_POST["pass"];
+    echo "$login  $password";
+}
+$user_form = '<form action="' . $_SERVER['PHP_SELF'] . '" method="post" name="autoForm">
+<input type="text" name="login" placeholder="Input login">
+<input type="password" name="pass" placeholder="Input password">
+<input type="submit" value="Go" name="go">
+</form>';
+
+echo $user_form;
