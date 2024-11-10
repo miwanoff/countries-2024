76d666ee81e9cf8cc6a64fc00dc940f9557b3f4a
miwanoff <marina.iva.noffa@gmail.com>
2024-11-10 13:03:48 +0200

    Add check_admin

--- action.php
+++ action.php
@@ -4,11 +4,22 @@ require_once "db.php";
 function check_autorize($log, $pass)
 {
     global $users;
-    $b = array_key_exists($log, $users) && $pass == $users[$log]['pass'];
-    return $b;
+    return array_key_exists($log, $users) && $pass == $users[$log]['pass'];
 }
 
 // if (check_autorize('rodger', 'qwerty455')) {
+//     echo "Yes";
+// } else {
+//     echo "No";
+// }
+
+function check_admin($log, $pass)  {
+    global $users;
+    //echo $users[$log]['role'];
+    return check_autorize($log, $pass) && $users[$log]['role'] == 'admin';
+}
+
+// if (check_admin('alex', 'admin2233')) {
 //     echo "Yes";
 // } else {
 //     echo "No";
