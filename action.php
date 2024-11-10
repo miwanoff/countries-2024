ee9e4a3ae0367616e7dfe8993faab4327fd2f11c
miwanoff <marina.iva.noffa@gmail.com>
2024-11-10 13:21:27 +0200

    Add sorting

--- action.php
+++ action.php
@@ -24,3 +24,76 @@ function check_admin($log, $pass)  {
 // } else {
 //     echo "No";
 // }
+
+function name($a, $b)
+{ // функция, определяющая способ сортировки (по названию столицы)
+    if ($a["capital"] < $b["capital"]) {
+        return -1;
+    } elseif ($a["capital"] == $b["capital"]) {
+        return 0;
+    } else {
+        return 1;
+    }
+
+}
+
+function area($a, $b)
+{ // функция, определяющая способ сортировки (по названию столицы)
+    if ($a["area"] < $b["area"]) {
+        return -1;
+    } elseif ($a["area"] == $b["area"]) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function population($a, $b)
+{ // функция, определяющая способ сортировки (по населению)
+    if ($a["population"]["2000"] + $a["population"]["2010"] < $b["population"]["2000"] + $b["population"]["2010"]) {
+        return -1;
+    } elseif ($a["population"]["2000"] + $a["population"]["2010"] == $b["population"]["2000"] + $b["population"]["2010"]) {
+        return 0;
+    } else {
+        return 1;
+    }
+
+}
+
+function sorting($how_to_sort)
+{
+    global $countries;
+    uasort($countries, $how_to_sort);
+}
+
+function out_arr()
+{
+    global $countries;
+    // делаем переменную $countries глобальной
+    $arr_out = [];
+    $arr_out[] = "<table>";
+    $arr_out[] = "<tr><td>№</td><td>Страна</td><td>Столица</td><td>Площадь</td><td>Население за 2000 год</td><td>Население за 2010 год</td><td>Среднее население</td></tr>";
+    foreach ($countries as $country) {
+        static $i = 1;
+        //статическая глобальная переменная-счетчик
+        $str = "<tr>";
+        $str .= "<td>" . $i . "</td>";
+        foreach ($country as $key => $value) {
+            if (!is_array($value)) {
+                $str .= "<td>$value</td>";
+            } else {
+                foreach ($value as $k => $v) {
+                    $str .= "<td>$v</td>";
+                }
+
+            }
+
+        }
+        $str .= "<td>" . (array_sum($country['population']) / count($country['population'])) . "</td>";
+        $str .= "</tr>";
+        $arr_out[] = $str;
+        $i++;
+    }
+    $arr_out[] = "</table>";
+    return $arr_out;
+}
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (isset($_POST["go"])) {
     if (check_autorize($login, $password)) {
         $autorized = true;
         echo "Hello, $login";
-        if (check_admin($login, $password)){
+        if (check_admin($login, $password)) {
             echo "<a href='hello.php?login=$login'>Просмотр отчета</a>";
         }
     } else {
@@ -22,4 +22,32 @@ $user_form = '<form action="' . $_SERVER['PHP_SELF'] . '" method="post" name="au
 <input type="submit" value="Go" name="go">
 </form>';
 
-echo $user_form;
+if (!$autorized) {
+    echo $user_form;
+}
+
+$str_form_s = '<h3>Сортировать по:</h3>
+<form action="index.php" method="post" name="sort_form">
+    <select name="sort" id="sort" size="1">
+        <option value="name">назва</option>
+        <option value="area">площа</option>
+        <option value="population">среднє населення</option>
+    </select>
+    <input type="submit" name="submit" value="OK">
+</form>';
+echo $str_form_s;
+
+if (isset($_POST["sort"])) {
+    $how_to_sort = $_POST["sort"];
+    sorting($how_to_sort);
+
+    $out = out_arr();
+
+    if (count($out) > 0) {
+        foreach ($out as $row) {
+            echo $row;
+        }
+    } else {
+        echo "Нет данных";
+    }
+}
