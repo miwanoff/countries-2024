0baceedc5291173dd47becb4a605c3ed17f9cfa7
miwanoff <marina.iva.noffa@gmail.com>
2024-11-10 12:52:02 +0200

    Add check_autorize

--- action.php
+++ action.php
@@ -1,2 +1,15 @@
 <?php
 require_once "db.php";
+
+function check_autorize($log, $pass)
+{
+    global $users;
+    $b = array_key_exists($log, $users) && $pass == $users[$log]['pass'];
+    return $b;
+}
+
+// if (check_autorize('rodger', 'qwerty455')) {
+//     echo "Yes";
+// } else {
+//     echo "No";
+// }
